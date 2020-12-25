dc817c378ef3e8c4dc6ec2ef8550680114e2fa6f
kkmeghan <kkmeghan@gmail.com>
2020-12-25 20:01:12 +0530

    Services

---
+++ ECommerce/ECommerce.API.Products/Profiles/ProductProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.API.Products.Profiles
+{
+    public class ProductProfile:AutoMapper.Profile
+    {
+
+        public ProductProfile()
+        {
+            CreateMap<Db.Product, Models.Product>();
+        }
+    }
+}
